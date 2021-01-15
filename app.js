571b5ac1db829abeb5aea92a9526a6f747712190
Your Name <adam@worleynet.co.uk>
2021-01-15 02:01:44 +0000

    tweaks

--- app.js
+++ app.js
@@ -1,14 +1,82 @@
-const http = require("http");
-const server = http.createServer();
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
+const User = require("./models/User");
 
-server.on("request", (req, res) => {
-  const { method, url, headers } = req;
 
-  console.log(method);
-  console.log(url);
-  console.log(headers);
+/**
+ * Controllers (route handlers).
+ */
 
-  res.end("hello world");
+
+const app = express();
+app.set("view engine", "ejs");
+
+/**
+ * notice above we are using dotenv. We can now pull the values from our environment
+ */
+
+const { PORT, MONGODB_URI } = process.env;
+
+/**
+ * connect to database
+ */
+
+
+
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+  console.log(
+    "MongoDB connection error. Please make sure MongoDB is running.",
+    chalk.red("✗")
+  );
+  process.exit();
 });
 
-server.listen(2020);
+/***
+ * We are applying our middlewear
+ */
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(expressSession({ secret: 'foo barr', cookie: { expires: new Date(253402300000000) } }))
+
+
+app.use("*", async (req, res, next) => {
+  global.user = false;
+  if (req.session.userID && !global.user) {
+    const user = await User.findById(req.session.userID);
+    global.user = user;
+  }
+  next();
+})
+
+const authMiddleware = async (req, res, next) => {
+  const user = await User.findById(req.session.userID);
+  if (!user) {
+    return res.redirect('/');
+  }
+  next()
+}
+
+app.get("/", homeController.list);
+
+app.get("/logout", async (req, res) => {
+  req.session.destroy();
+  global.user = false;
+  res.redirect('/');
+})
+
+app.listen(PORT, () => {
+  console.log(
+    `Example app listening at http://localhost:${PORT}`,
+    chalk.green("✓")
+  );
+});
