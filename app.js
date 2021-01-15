6c407d19cb8b22eef71f1d5035b16f564edd59cf
Your Name <adam@worleynet.co.uk>
2021-01-15 10:38:25 +0000

    User login created

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
+const bodyParser = require("body-parser");
+const expressSession = require("express-session");
 
 /**
  * Controllers (route handlers).
  */
 const infectionController = require("./controllers/infection");
+const userController = require("./controllers/user");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -33,6 +36,35 @@ mongoose.connection.on("error", (err) => {
 });
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(expressSession({ secret: 'foo barr', cookie: { expires: new Date(253402300000000) } }))
+
+app.use("*", async (req, res, next) => {
+  global.user = false;
+  if (req.session.userID && !global.user) {
+    const user = await User.findById(req.session.userID);
+    global.user = user;
+  }
+  next();
+})
+
+const authMiddleware = async (req, res, next) => {
+  const user = await User.findById(req.session.userID);
+  if (!user) {
+    return res.redirect('/');
+  }
+  next()
+}
+
+app.get("/logout", async (req, res) => {
+  req.session.destroy();
+  global.user = false;
+  res.redirect('/');
+})
+
+
 app.get("/", (req, res) => {
   res.render("index");
 });
